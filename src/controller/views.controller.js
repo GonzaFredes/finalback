c78c0ce307b199c9b285bf7b66f8b54fd4e56018
Gonzalo Fredes <104870034+GonzaFredes@users.noreply.github.com>
2023-02-17 19:37:53 -0300

    Arreglo paginacion en home

--- src/controller/views.controller.js
+++ src/controller/views.controller.js
@@ -3,9 +3,10 @@ const ProductManager = require("../dao/mongoManager/BdProductManager");
 
 
 const views = async (req, res) => {
-    const products = await ProductManager.getProduct();
+    const page = req.query.page
+    const products = await ProductManager.getProduct(page);
     const view = products.docs.map((products) => ({ title: products.title, description: products.description, price: products.price, stock: products.stock, thumbnail: products.thumbnail}));
-    res.render('home', { products: view, hasPrevPage: !products.hasPrevPage, hasNextPage: !products.hasNextPage, page: products.page, totalPages: products.totalPages, prevPage: products.prevPage, nextPage: products.nextPage});
+    res.render('home', { products: view, hasPrevPage: products.hasPrevPage, hasNextPage: products.hasNextPage, page: products.page, totalPages: products.totalPages, prevPage: products.prevPage, nextPage: products.nextPage});
 }
 
 const viewCart = async (req, res) => {
